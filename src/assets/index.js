f31483cd23eca34215f5fbbd9b6517b1f35d8202
Jack-Camo <jackecam@gmail.com>
2025-01-21 19:21:12 +0000

    asset: add hero assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,11 @@
 import menu from "./menu.svg";
 import close from "./close.svg";
 import logo_gold from './outstanding-property-logo-rectangle-gold.png';
-
+import lake_district_langdales from './lake-district-langdales.jpg';
 
 export {
     logo_gold,
     menu,
-    close
+    close,
+    lake_district_langdales
 };
